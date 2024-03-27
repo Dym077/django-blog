e043e175c8f2e79ffea8f3a6790c7dd738032c62
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-03-27 12:38:20 +0000

    Add comment meta class

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
+        
     def __str__(self):
         return f"The title of this post is {self.title} | written by {self.author}"
 
@@ -28,3 +29,11 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+ 
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"    
+
