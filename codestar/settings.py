04cb349e1cb63f5d7c6aa15b8b87c518b5b93aee
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-03-29 17:59:15 +0000

    List view filters

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-dym077-djangoblog-h78tkd67ihe.ws-eu110.gitpod.io','.herokuapp.com']
 
