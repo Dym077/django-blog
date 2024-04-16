9c57b4674a8ad7521c0cb562f2a3c0cfaed67e73
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-16 14:56:54 +0000

    Create about page

---
+++ about/admin.py
@@ -0,0 +1,12 @@
+
+# Register your models here.
+
+from django.contrib import admin
+from .models import About
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content',)
